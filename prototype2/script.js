72df21220133b3a42fe739287f48d9ffc6c58844
YTAzzy <>
2025-02-02 19:55:18 -0500

    Flatland Prototype

--- prototype2/script.js
+++ prototype2/script.js
@@ -32,7 +32,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 scene.add(camera)
-camera.position.set(0, 0, 5)
+camera.position.set(-2, 3, 5)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -57,12 +57,65 @@ const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
 scene.add(testSphere)
 
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 75, 75)
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = Math.PI * 0.5
+
+scene.add(plane)
+
+
 /**
  * UI
  */
 
 //UI
 const ui = new dat.GUI()
+
+//UI Object
+const uiObject = {
+    speed: 1,
+    distance: 1,
+    scale: 1,
+    play: false
+}
+
+
+//Plane UI
+const planeFolder = ui.addFolder('Plane')
+planeFolder.add(planeMaterial, 'wireframe')
+
+//Sphere UI
+const sphereFolder = ui.addFolder('Sphere')
+
+sphereFolder
+    .add(uiObject, 'speed')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Speed')
+
+sphereFolder
+    .add(uiObject, 'distance')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Distance')
+
+sphereFolder   
+    .add(uiObject, 'scale')
+    .min(0.1)
+    .max(10)
+    .step(0.1)
+    .name('Scale')
+
+sphereFolder
+    .add (uiObject, 'play')
+    .name('Animate')
 
 /*******************
 ** ANIMATION LOOP **
@@ -73,6 +126,14 @@ const animation = () =>
 {
     // Return elapsedTime
     const elapsedTime = clock.getElapsedTime()
+
+    //Animate Sphere
+    if (uiObject.play){
+        testSphere.position.y = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
+    }
+    testSphere.scale.x = uiObject.scale
+    testSphere.scale.y = uiObject.scale
+    testSphere.scale.z = uiObject.scale
 
     //Update Orbit Controls
     controls.update()
